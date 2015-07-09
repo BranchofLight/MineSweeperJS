7c2a0a78374fe13deb1aefa0c7dba25c74ccb5ea
Scott Andrechek <scottandrechek@gmail.com>
2015-07-08 21:09:57 -0400

    Setup Grunt and added skeletons

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        seperator: ',',
+      },
+      dist: {
+        src: ['src/*.js'],
+        dest: 'dist/build.js',
+      },      
+    },
+  });
+
+  // Load the plugin that provides the "concat" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat']);
+};
---
+++ src/index.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$('p').css('color', 'green');
+	console.log("Hello World!");
+});
